6b67dc51951698e87da60eac08aaaab354acd8fb
admin <admin@admins-MacBook-Pro.local>
2024-07-02 13:57:07 +0400

    comments

--- src/core/auth/index.tsx
+++ src/core/auth/index.tsx
@@ -1,5 +1,5 @@
 import { create } from 'zustand';
-import { devtools, persist } from 'zustand/middleware';
+// import { devtools, persist } from 'zustand/middleware';
 
 import { createSelectors } from '../utils';
 import type { TokenType } from './utils';
